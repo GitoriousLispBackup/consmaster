f47c20ebfda8542d59af6cddff43e5dd3d20c32f
yoch <yoch.melka@gmail.com>
2013-05-13 22:42:26 +0300

    add basic interpreter

---
+++ src/cm_interpreter.py
@@ -0,0 +1,24 @@
+from pylisp.lisp_lexer import lisp_lexer
+from pylisp.lisp_parser import lisp_parser
+from pylisp.lisp_errors import LispError, LispParseError
+from pylisp.lisp import _Fvals, nil
+
+class Interpreter:
+    counter = 0
+    def __new__(cls):
+        if Interpreter.counter != 0:
+            raise RuntimeError('unable to create more thant one interpreter')
+        Interpreter.counter = 1
+        return super().__new__(cls)
+    def __init__(self):
+        self.namespace = {}
+        self.namespace['nil'] = nil
+        _Fvals.clear()
+    def eval(self, expr):
+        expr = lisp_parser.parse(expr, lexer=lisp_lexer)[0]
+        return expr.eval(self.namespace)
+    def getFromEnv(self, symbol):
+        return self.namespace.get(symbol)
+    def __del__(self):
+        Interpreter.counter = 0
+
