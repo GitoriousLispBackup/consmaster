83f8a27e076936fa466298f1dfc8849fa8c72ad4
yoch <yoch.melka@gmail.com>
2013-06-14 15:39:20 +0300

    ajout du module de suivi étudiant

---
+++ src/cm_monitor.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class ExoType:
+    def __init__(self):
+        self.training = defaultdict(list)
+        self.exercices = defaultdict(dict)
+    def current_level(self):
+        for lvl in sorted(self.training.keys(), reverse=True):
+            results = self.training[lvl]
+            if len(results) >= 10 and sum(results[-10:]) >= 7:
+                return lvl + 1
+        return 0
+    def __repr__(self):
+        return 'ExoType(training=' + repr(self.training) + ', exercices=' + repr(self.exercices) + ')'
+
+class UserData:
+    def __init__(self, name, mail, nmodes=3):
+        self.name = name
+        self.mail = mail
+        self.modes = [ExoType() for _ in range(nmodes)]
+    def get_mode(self, n):
+        return self.modes[n]
+    def __repr__(self):
+        return '<UserData: ' + self.name + ', ' + self.mail + '>'
