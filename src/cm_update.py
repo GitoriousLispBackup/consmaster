88f104238a9c50f922ad775584aa7d5d9dd23881
yoch <yoch.melka@gmail.com>
2013-08-25 01:16:35 +0300

    minor changes in update functions

--- src/cm_update.py
+++ src/cm_update.py
@@ -9,7 +9,10 @@ from cm_exercice import decoder
 
 
 def update_bdd():
-    raw_exos = get_exercices(); print(raw_exos)
-    dct = {uid: json.loads(serialized, object_hook=decoder) for uid, serialized in raw_exos.items()}
-    CM_BDD.update(dct); print(dct)
-    CM_BDD.sync()
+    raw_exos = get_exercices(); # print(raw_exos)
+    # TODO: prevent if unable to connect to network
+    if raw_exos.keys() > CM_BDD.keys():
+        print('news exercices available') # TODO: change this
+        dct = {uid: json.loads(serialized, object_hook=decoder) for uid, serialized in raw_exos.items() if uid not in CM_BDD}
+        CM_BDD.update(dct); # print(dct)
+        CM_BDD.sync()
