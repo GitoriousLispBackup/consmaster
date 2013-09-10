fc243c35c5686167096c7cf2c8e1cccea9f7f523
yoch <yoch.melka@gmail.com>
2013-09-10 15:31:10 +0300

    fix update exercices in client

--- src/cm_update.py
+++ src/cm_update.py
@@ -3,16 +3,23 @@
 
 import json
 
+try:
+    from PySide.QtCore import *
+    from PySide.QtGui import *
+except:
+    print ("Error: This program needs PySide module.", file=sys.stderr)
+    sys.exit(1)
+
 from cm_globals import CM_BDD
 from cm_connexion import *
-from cm_exercice import decoder
+from cm_exercice import ex_loads
 
 
 def update_bdd():
-    raw_exos = get_exercices(); # print(raw_exos)
+    raw_exos = get_exercices(); print(raw_exos)
     # TODO: prevent if unable to connect to network
-    if raw_exos.keys() > CM_BDD.keys():
-        QMessageBox.information(self, "Info", "De nouveaux exercices sont disponibles.")
-        dct = {uid: json.loads(serialized, object_hook=decoder) for uid, serialized in raw_exos.items() if uid not in CM_BDD}
+    if raw_exos.keys().isdisjoint(CM_BDD.keys()):
+        QMessageBox.information(None, "Info", "De nouveaux exercices sont disponibles.")
+        dct = {uid: ex_loads(serialized) for uid, serialized in raw_exos.items() if uid not in CM_BDD}
         CM_BDD.update(dct); # print(dct)
         CM_BDD.sync()
